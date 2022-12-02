96b5e95fb526f564410e72db5e265fa64a8ac490
Desrine <113426263+Desrine@users.noreply.github.com>
2022-12-02 10:49:10 +0530

    2 files modified

--- src/app/parent1/parent.module.ts
+++ src/app/parent1/parent.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Child1Component } from './child1/child1.component';
 import { C1child2Component } from './c1child2/c1child2.component';
+import { Parent2Module } from '../parent2/parent2.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, Parent2Module],
   declarations: [Child1Component, C1child2Component],
   exports: [Child1Component],
 })
--- src/app/parent2/parent2.module.ts
+++ src/app/parent2/parent2.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Child2Component } from './child2/child2.component';
+import { ParentModule } from '../parent1/parent.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [Child2Component]
+  imports: [CommonModule],
+  declarations: [Child2Component],
 })
-export class Parent2Module { }
+export class Parent2Module {}
